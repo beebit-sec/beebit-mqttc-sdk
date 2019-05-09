5c3064893c0fbb66e5e2997e47e2c894171f77be
Wei-Tsung Su <ellington.su@gmail.com>
2019-05-09 10:35:19 +0800

    add name in cpabe option (#8)
    * go to work
    
    * add README
    
    * add README; modulization design
    
    * add create_mqtt_tts_msg()
    
    * add lower mechanism
    
    * README: how to add security mechanism
    
    * bugfix
    
    * add name in cpabe option

--- paho.mqtt.c/src/beebit_cpabe.h
+++ paho.mqtt.c/src/beebit_cpabe.h
@@ -10,11 +10,12 @@ extern int beebit_cpabe_encode(const BeebitOptions*, char*, int, char**);
 extern int beebit_cpabe_decode(const BeebitOptions*, char*, int, char**);
 
 typedef struct {
+	const char name[8];	
 	const char* pk; // public key
 	const char* sk; // secret key
 	const char* ap; // access policy
 } BeebitCPABEOptions;
 
-#define BeebitCPABEOptions_initializer {NULL, NULL, NULL}
+#define BeebitCPABEOptions_initializer {{'A','C','_','C','P','A','B','E'}, NULL, NULL, NULL}
 
 #endif
