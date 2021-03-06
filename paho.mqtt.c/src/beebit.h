1aeb446029481862851fb97b14d34fe333613fc2
ai090935 <79894213+ai090935@users.noreply.github.com>
2021-03-07 01:55:58 +0800

    Update beebit.h

--- paho.mqtt.c/src/beebit.h
+++ paho.mqtt.c/src/beebit.h
@@ -18,13 +18,65 @@ typedef struct {
 extern void init_beebit();
 extern int create_mqtt_tts_msg(unsigned char sec, char*payload, int payloadlen, char** dst);
 typedef int (*beebit_handler)(BeebitOptions*, char*, int, char**);
-extern beebit_handler beebit_handler_map[256][2];
+
+typedef struct {
+	beebit_handler encode;
+	beebit_handler decode;
+	unsigned char code;
+}aaa;
+
+typedef struct _aaa_list_data
+{
+	aaa value;
+	struct _aaa_list_data* next;
+}aaa_list_data;
+
+typedef struct
+{
+	aaa_list_data* begin;
+
+	aaa_list_data* lpCurrent;
+}aaa_list;
+
+extern aaa_list beebit_handler_map;
+
+#include<stdlib.h>
+inline static void aaa_list_push_front(aaa value)
+{
+	aaa_list_data* p = malloc(sizeof(aaa_list_data));
+	if (!p)
+		exit(-1);//bad alloc
+
+	p->value = value;
+	p->next = beebit_handler_map.begin;
+
+	beebit_handler_map.begin = p;
+}
+
+inline static void aaa_list_iterator_constructor()
+{
+	beebit_handler_map.lpCurrent = beebit_handler_map.begin;
+}
+
+inline static aaa_list_data* aaa_list_iterator_notDone()
+{
+	return beebit_handler_map.lpCurrent;
+}
+
+inline static void aaa_list_iterator_next()
+{
+	beebit_handler_map.lpCurrent = beebit_handler_map.lpCurrent->next;
+}
+
+inline static aaa* aaa_list_iterator_currentItem()
+{
+	return &beebit_handler_map.lpCurrent->value;
+}
+
 extern int get_mqtt_tts_tl_byte_number(char* src);
 extern int get_mqtt_tts_tl(char* src);
 
 #define BeebitOptions_initializer {{'B','E','E','B','I','T'}, 0, 0, NULL}
-#define ENCODE 0
-#define DECODE 1
 #include "beebit_extend.h"
 
 #endif
